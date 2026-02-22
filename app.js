daa46adc5227b54691df7dd4d358502d0b4235d5
Sarthak R. Katare <kataresarthak001@gmail.com>
2026-02-22 18:05:13 +0530

    Add listingRouter to the root route

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", listingRouter); 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
